ac7b02435961ed7ef6f027503a7a16664aa60e3a
Pathithasan <rpathithasan0@gmail.com>
2023-04-28 16:53:35 +0530

    Cafe id make it as intiger and AUTO_INCREMENT

--- MyCafe_DataBase/CafeDbScript.sql
+++ MyCafe_DataBase/CafeDbScript.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE cafedb;
 Use cafedb;
 CREATE TABLE cafe (
-  id CHAR(36) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL UNIQUE,
   description VARCHAR(255) NOT NULL,
   logo VARCHAR(255),
@@ -9,14 +9,13 @@ CREATE TABLE cafe (
   PRIMARY KEY (id)
 );
 
-
 CREATE TABLE employee (
   id CHAR(9) NOT NULL,
   name VARCHAR(255) NOT NULL UNIQUE,
   email_address VARCHAR(255) NOT NULL UNIQUE,
   phone_number VARCHAR(8) NOT NULL UNIQUE,
   gender ENUM('Male', 'Female') NOT NULL,
-  cafe_id CHAR(36) NOT NULL,
+  cafe_id int NOT NULL,
   start_date DATE NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (cafe_id) REFERENCES cafe(id)
